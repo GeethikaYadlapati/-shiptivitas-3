840a73a32ebdbf23c57e78b70c50fe0fa6bd5bde
Geethika Yadlapati <ygeethika2002@gmail.com>
2026-03-27 04:30:14 -0700

    Wrote sql queries to map daily avg users before vs after feature change and indicate number of status changes by card

--- answer.sql
+++ answer.sql
@@ -2,12 +2,31 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
+SELECT date(login_timestamp, 'unixepoch') AS Day, COUNT(DISTINCT user_id) AS NumUsers
+FROM login_history WHERE login_timestamp < strftime('%s', '2018-06-02') GROUP BY Day;
 
+SELECT AVG(NumUsers) AS dau_beforefeature FROM (
+    SELECT date(login_timestamp, 'unixepoch') AS Day, COUNT(DISTINCT user_id) AS NumUsers 
+    FROM login_history 
+    WHERE login_timestamp < strftime('%s', '2018-06-02') 
+    GROUP BY Day
+);
 
+SELECT date(login_timestamp, 'unixepoch') AS Day, COUNT(DISTINCT user_id) AS NumUsers
+FROM login_history WHERE login_timestamp >= strftime('%s', '2018-06-02') GROUP BY Day;
 
+SELECT AVG(NumUsers) AS dau_afterfeature FROM(
+    SELECT date(login_timestamp, 'unixepoch') AS Day, COUNT(DISTINCT user_id) AS NumUsers 
+    FROM login_history 
+    WHERE login_timestamp >= strftime('%s', '2018-06-02') 
+    GROUP BY Day
+);
 -- PART 2: Create a SQL query that indicates the number of status changes by card
 
-
+SELECT name, cardID, COUNT(card_change_history.id) 
+FROM card_change_history 
+JOIN card ON card_change_history.id = card.id 
+WHERE oldStatus is NOT NULL GROUP BY cardID;
 
 
 
